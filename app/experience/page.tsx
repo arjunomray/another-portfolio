3edf2ea8ca4fc9c4a1d10f0d1c46669b550f4e50
arjunomray <arjunomray@gmail.com>
2025-01-24 10:45:28 +0000

    feat: add hover animations

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -13,27 +13,29 @@ export default function Experiences() {
       <h1 className="mb-8 text-2xl font-medium tracking-tight">Experiences</h1>
       <div className="space-y-6">
         {experiences.map((experience, index) => (
-          <a
-            key={index}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="block group hover:opacity-80 transition-opacity duration-200"
-          >
-            <div className="flex flex-col">
-              <div className="w-full flex justify-between items-baseline">
-                <span className="text-black dark:text-white font-medium tracking-tight">
-                  {experience.company}
-                </span>
-                <span className="text-neutral-600 dark:text-neutral-400 tabular-nums text-sm">
-                  {experience.duration}
-                </span>
+          <div className="p-2 dark:shadow-white rounded-lg shadow-md hover:shadow-2xl hover:scale-105 transform transition-all duration-300">
+            <a
+              key={index}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="block group hover:opacity-80 transition-opacity duration-200"
+            >
+              <div className="flex flex-col">
+                <div className="w-full flex justify-between items-baseline">
+                  <span className="text-black dark:text-white font-medium tracking-tight">
+                    {experience.company}
+                  </span>
+                  <span className="text-neutral-600 dark:text-neutral-400 tabular-nums text-sm">
+                    {experience.duration}
+                  </span>
 
+                </div>
+                <span className="text-neutral-600 dark:text-neutral-400 text-sm  font-light italic tracking-tight">
+                  {experience.position}
+                </span>
               </div>
-              <span className="text-neutral-600 dark:text-neutral-400 text-sm  font-light italic tracking-tight">
-                {experience.position}
-              </span>
-            </div>
-          </a>
+            </a>
+          </div>
         ))}
       </div>
     </section>
--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Page() {
         <Image
           src="/profile.png"
           alt="Profile photo"
-          className="rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5 hover:grayscale-0"
+          className=" dark:shadow-white shadow-lg hover:shadow-2xl hover:scale-105 transform transition-all duration-300 rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5 hover:grayscale-0"
           unoptimized
           width={160}
           height={160}
